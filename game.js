fb2cecfae35d6c17b903fcef3f482deab94d511a
vladimirfomene <vladimirfomene@gmail.com>
2020-02-03 12:30:16 +0100

    implement setupGame

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
 function startGame () {
   const players = setupPlayers();
   const grid = buildBoard();
-  setupGame(grid, players);
+  setupGame(players);
   while (!isGameOver(grid)) {
-    const currentPlayer = choosePlayer();
+    const currentPlayer = choosePlayer(players);
     play(currentPlayer, grid);
     printBoard(grid);
   }
@@ -20,12 +27,14 @@ function setupPlayers () {
     {
       type: "human",
       character: "",
-      hasWon: false
+      hasWon: false,
+      turn: false
     },
     {
       type: "AI",
       character: "",
-      hasWon: false
+      hasWon: false,
+      turn: false
     }
   ];
 }
@@ -45,6 +54,26 @@ function buildBoard () {
   return grid;
 }
 
+function setupGame (players) {
+  console.log("Welcome! This is a Tic Tac Toe game.");
+  console.log("To start playing you need to choose a character you will use from either \"o\" or \"x\"");
+
+  let char = "";
+  rl.question("Enter your chosen character? ", function (ans) {
+    char = ans.trim();
+    rl.close();
+
+    if (char === "o") {
+      players[0].character = char;
+      players[1].character = "x";
+    } else {
+      players[0].character = char;
+      players[1].character = "o";
+    }
+  });
+}
+
+exports.setupGame = setupGame;
 exports.buildBoard = buildBoard;
 exports.setupPlayers = setupPlayers;
 exports.startGame = startGame;
