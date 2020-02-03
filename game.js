5fd47ce079d171a4c83281b824626c95a35cec1c
vladimirfomene <vladimirfomene@gmail.com>
2020-02-03 17:29:35 +0100

    implement choosePosition and remove readline code

--- game.js
+++ game.js
@@ -1,11 +1,6 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
+const readline = require("readline-sync");
 
-function startGame() {
+function startGame () {
   const players = setupPlayers();
   const grid = buildBoard();
   setupGame(players);
@@ -17,12 +12,12 @@ function startGame() {
   declareWinner();
 }
 
-function play(player, grid) {
-  const position = choosePosition();
+function play (player, grid) {
+  const position = choosePosition(player);
   updateBoard(grid, position, player);
 }
 
-function setupPlayers() {
+function setupPlayers () {
   return [
     {
       type: "human",
@@ -39,7 +34,7 @@ function setupPlayers() {
   ];
 }
 
-function buildBoard() {
+function buildBoard () {
   const grid = new Array(3);
   for (let k = 0; k < grid.length; k++) {
     grid[k] = new Array(3);
@@ -54,14 +49,14 @@ function buildBoard() {
   return grid;
 }
 
-function setupGame(players) {
+function setupGame (players) {
   console.log("Welcome! This is a Tic Tac Toe game.");
   console.log(
-    'To start playing you need to choose a character you will use from either "o" or "x"'
+    "To start playing you need to choose a character you will use from either \"o\" or \"x\""
   );
 
   let char = "";
-  rl.question("Enter your chosen character? ", function(ans) {
+  rl.question("Enter your chosen character? ", function (ans) {
     char = ans.trim();
     rl.close();
 
@@ -75,7 +70,7 @@ function setupGame(players) {
   });
 }
 
-function isGameOver(grid, players) {
+function isGameOver (grid, players) {
   for (let i = 0; i < grid.length; i++) {
     if (grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]) {
       for (const player of players) {
@@ -118,7 +113,7 @@ function isGameOver(grid, players) {
   return false;
 }
 
-function isGridFull(grid) {
+function isGridFull (grid) {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
       if (typeof grid[i][j] === "number") return false;
@@ -127,7 +122,7 @@ function isGridFull(grid) {
   return true;
 }
 
-function printBoard(grid, printFn) {
+function printBoard (grid, printFn) {
   for (let i = 0; i < grid.length; i++) {
     let row = "";
     for (let j = 0; j < grid[0].length; j++) {
@@ -137,7 +132,7 @@ function printBoard(grid, printFn) {
   }
 }
 
-function choosePlayer(players) {
+function choosePlayer (players) {
   if (!players[0].turn && !players[1].turn) {
     const idx = Math.round(Math.random() * 1);
     players[idx].turn = true;
@@ -159,6 +154,22 @@ function choosePlayer(players) {
   return null;
 }
 
+function choosePosition (player) {
+  let position = -1;
+  if (player.type === "human") {
+    while (position < 1 || position > 9 || isNaN(position)) {
+      position = parseInt(readline.question("Choose a position on the board? "));
+    }
+  }
+
+  if (player.type === "AI") {
+    position = Math.round(Math.random() * 8) + 1;
+  }
+
+  return position;
+}
+
+exports.choosePosition = choosePosition;
 exports.choosePlayer = choosePlayer;
 exports.printBoard = printBoard;
 exports.isGameOver = isGameOver;
