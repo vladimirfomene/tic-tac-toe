922923b2af7e3f268c037b46eba2308bc05e9723
vladimirfomene <vladimirfomene@gmail.com>
2020-02-03 08:20:49 +0100

    implement buildBoard for grid creation

--- game.js
+++ game.js
@@ -1,21 +1,21 @@
-function startGame() {
+function startGame () {
   const players = setupPlayers();
   const grid = buildBoard();
   setupGame(grid, players);
   while (!isGameOver(grid)) {
-    let currentPlayer = choosePlayer();
+    const currentPlayer = choosePlayer();
     play(currentPlayer, grid);
     printBoard(grid);
   }
   declareWinner();
 }
 
-function play(player, grid) {
-  let position = choosePosition();
+function play (player, grid) {
+  const position = choosePosition();
   updateBoard(grid, position, player);
 }
 
-function setupPlayers() {
+function setupPlayers () {
   return [
     {
       type: "human",
@@ -30,6 +30,22 @@ function setupPlayers() {
   ];
 }
 
+function buildBoard () {
+  const grid = new Array(3);
+  for (let k = 0; k < grid.length; k++) {
+    grid[k] = new Array(3);
+  }
+
+  let count = 1;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      grid[i][j] = count++;
+    }
+  }
+  return grid;
+}
+
+exports.buildBoard = buildBoard;
 exports.setupPlayers = setupPlayers;
 exports.startGame = startGame;
 exports.play = play;
